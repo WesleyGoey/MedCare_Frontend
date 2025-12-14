935beac17c7e916b53d5b8e9af48a7cf72806be6
WesleyGoey <wesley.goey@gmail.com>
2025-12-14 14:44:02 +0700

    w

--- app/src/main/java/com/wesley/medcare/data/service/MedicineService.kt
+++ app/src/main/java/com/wesley/medcare/data/service/MedicineService.kt
@@ -1,4 +1,11 @@
 package com.wesley.medcare.data.service
 
 interface MedicineService {
+
+
+
+//    @GET("search.php")
+//    suspend fun getArtist(
+//        @Query("s") artistName: String
+//    ): Response<ResponseArtist>
 }
