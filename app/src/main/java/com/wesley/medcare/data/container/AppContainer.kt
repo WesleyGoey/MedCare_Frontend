61cdeaca53036e0d8328417ee190f9686bf71586
WesleyGoey <wesley.goey@gmail.com>
2026-01-12 21:44:18 +0700

    p

--- app/src/main/java/com/wesley/medcare/data/container/AppContainer.kt
+++ app/src/main/java/com/wesley/medcare/data/container/AppContainer.kt
@@ -16,12 +16,7 @@ import retrofit2.converter.gson.GsonConverterFactory
 class AppContainer(private val context: Context) {
     companion object {
 
-//        const val ROOT_URL = "http://10.50.164.93:3000" //hotspot wesley
-//        const val ROOT_URL = "http://192.168.1.15:3000" //wifi wesley
-        const val ROOT_URL = "http://10.0.182.142:3000" //staff uc
-//        const val ROOT_URL = "http://172.20.10.4:3000" //hotspot niki
-//        const val ROOT_URL = "http://192.168.1.104:3000" //wifi niki
-//        const val ROOT_URL = "http://192.168.1.72:3000" //mrn
+        const val ROOT_URL = "http://10.0.182.142:3000"
         const val BASE_URL = "${ROOT_URL}/api/"
     }
 
